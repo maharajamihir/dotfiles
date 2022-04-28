f68d6ff425b0020713cd846623a85e97b51601e4
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2022-04-28 20:43:50 +0200

    more config

--- .bashrc
+++ .bashrc
@@ -6,20 +6,21 @@
 [[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
+
 PS1='[\u@\h \W]\$ '
 alias secure_config='cp -r ~/.config/ ~/Projects/archconfig/'
 
 # opam configuration
 test -r /home/maharajamihir/.opam/opam-init/init.sh && . /home/maharajamihir/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
 
-
+alias hello="echo Sup"
+alias z="zathura --fork $@"
 
 # folder nvaigation
 alias projects="cd ~/Projects/"
 alias uni="cd ~/Uni/"
-alias fpv="cd ~/Uni/3_Sem/FPV/"
-alias gbs="cd ~/Uni/3_Sem/GBS/"
-alias GDB="cd ~/Uni/3_Sem/GDB/"
-alias ana="cd ~/Uni/3_Sem/Analysis/"
-alias tm="cd ~/Uni/3_Sem/TM/"
+alias grnvs="cd ~/Uni/4_Sem/GRNVS/"
+alias theo="cd ~/Uni/4_Sem/Theo/"
+alias dwt="cd ~/Uni/4_Sem/DWT/"
+alias rt="cd ~/Uni/4_Sem/Regelungstechnik/"
 alias cad="cd ~/Uni/3_Sem/CAD/"
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -55,7 +55,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -85,7 +85,7 @@
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
@@ -93,9 +93,9 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
@@ -112,7 +112,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       cc                ; C > C++ == 1
+       (cc +lsp)                ; C > C++ == 1
        clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -135,12 +135,12 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       json              ; At least it ain't XML
        (java +meghanada) ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
@@ -152,7 +152,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
